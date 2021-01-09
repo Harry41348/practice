6e6c113fd58d428c3e7e29546be3fa2e4f6fab35
Harry Wijnschenk <hvalentinow@gmail.com>
2021-01-09 21:30:22 +0000

    Basic PHP file

---
+++ PHP/freecodecamp/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice</title>
+</head>
+<body>
+    <?php
+
+        $name = "Harry";
+
+        echo "<h1>$name's Webpage</h1>";
+        echo "<hr>";
+        echo "<p>First PHP website outside of University</p>";
+
+    ?>
+</body>
+</html>
