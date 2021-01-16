a7afcc098f186e18e0812bb0cb63cf65439385f3
Harry Wijnschenk <hvalentinow@gmail.com>
2021-01-16 20:43:55 +0000

    Connecting database
    Database connection file with basic HTML

---
+++ PHP/database-tutorial/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+require_once 'includes/database.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Test</h1>
+<?php 
+
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+$rowCount = mysqli_num_rows($result);
+
+if ($rowCount > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row['username']. "<br>";
+    }
+} else {
+    echo "No results fouund.";
+}
+
+?>
+</body>
+</html>
